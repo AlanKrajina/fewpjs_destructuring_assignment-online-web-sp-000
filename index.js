26183373a81f3237b74d83fc6d4fd2d12833527a
Alan Krajina <alankrajina88@gmail.com>
2019-11-07 15:20:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ const [,,,,,indg,] = colors
 // Objects
 
 const { name, color, song, job, partner } = muppet;
-const { song_2: song_2, song_4: song_4 } = k_muppet.k_album.the_muppet_movie;
+const { k_album: {song_2: song_2, song_4: song_4}, k_job: k_job, k_partner: k_partner  } = k_muppet4;
