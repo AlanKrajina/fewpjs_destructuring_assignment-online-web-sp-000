c048dfb8d94cf258c136ab22ae98e3ddcb250dd9
Alan Krajina <alankrajina88@gmail.com>
2019-11-07 15:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,11 +37,4 @@ const [red,orange,yellow,green,blue,indigo,violet] = colors
 const [r,o,y,g,b, ,v] = colors
 const [,,,,,indg,] = colors
 
-
-
 // Objects
-
-// 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
